f69b791062eae0d007564ece244e2a9361920269
Ozgur Sar <ozgursar77@gmail.com>
2023-02-03 16:51:56 +0300

    Added theme selector

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-// Dashboard scripts
+const rootEl = document.querySelector(":root")
+const selectEl = document.querySelector("#theme_options")
+
+
+const handleChange = (e) => {
+  rootEl.className = e.target.value  
+}
+
+selectEl.addEventListener('change', handleChange)
